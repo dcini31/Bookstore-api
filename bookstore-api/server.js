c9e8eaec4ce0d4271d6a4c68f08c1c28b7f36c68
dcini31 <73096398+dcini31@users.noreply.github.com>
2024-05-14 23:04:37 +0200

    added CORS error handling

--- bookstore-api/server.js
+++ bookstore-api/server.js
@@ -13,7 +13,7 @@ const sequelize = new Sequelize("Bookstore", "Daniel", "20naD5013*", {
 //Defines Models
 const models = initModels(sequelize);
 
-// Initialize and Test Connection (optional)
+// Initialize and Test Connection
 (async () => {
   try {
     await sequelize.authenticate();
@@ -22,6 +22,21 @@ const models = initModels(sequelize);
     console.error("Unable to connect to the database:", error);
   }
 })();
+
+//CORS error handling
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  //checks options for request
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 // Customers API Endpoint
 app.get("/api/customers", async (req, res) => {
@@ -79,6 +94,23 @@ app.get("/api/orders", async (req, res) => {
     console.error("Error fetching orders:", error);
     res.status(500).json({ error: "Internal server error" });
   }
+});
+
+//setting error handling middleware
+app.use((req, res, next) => {
+  const error = new Error("404 NOT FOUND");
+  error.status = 404;
+  next(error);
+});
+
+//handles all errors
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
 const port = 3000;
